c72a443066bb047663fdbd0641988b5b23cd09df
David Scandurra <31861387+SplotyCode@users.noreply.github.com>
2025-12-22 00:12:45 +0100

    Create project through IntelliJ gradle kotlin setup

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.2.21"
+}
+
+group = "de.scandurra"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(23)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
